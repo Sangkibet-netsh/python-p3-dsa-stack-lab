ddc69d5a88d453024a49e8fdd57fde6e389739ee
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-06-12 01:22:54 +0300

    Complete

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,33 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
-
-    def isEmpty(self):
-        pass
+    def __init__(self, items=None, max_size=None):
+        if items is None:
+            items = []
+        self.items = items
+        self.max_size = max_size
 
     def push(self, item):
-        pass
+        if self.max_size is not None and len(self.items) >= self.max_size:
+            raise Exception("Stack is full")
+        self.items.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        return self.items.pop()
 
-    def peek(self):
-        pass
-    
     def size(self):
-        pass
+        return len(self.items)
+
+    def isEmpty(self):
+        return self.size() == 0
 
     def full(self):
-        pass
+        if self.max_size is None:
+            return False
+        return self.size() >= self.max_size
 
-    def search(self, target):
-        pass
+    def search(self, item):
+        if item not in self.items:
+            return -1
+        return self.size() - self.items.index(item) - 1
